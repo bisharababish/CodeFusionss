508226e59185314479ce2deb187a018ac4720c68
bisharababish <62569803+bisharababish@users.noreply.github.com>
2025-03-14 01:30:43 +0200

    s

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -130,7 +130,7 @@ const SocialLink = styled.a`
   }
   
   @media (min-width: 768px) and (max-width: 1024px) {
-    padding: 0.2rem 0;
+    padding: 0.5rem 0;
   }
   
   @media (max-width: 768px) {

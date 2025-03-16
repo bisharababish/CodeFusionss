b83416a647a7a0817db14cd3619904ccec83810a
bisharababish <62569803+bisharababish@users.noreply.github.com>
2025-03-16 12:54:33 +0200

    s

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,7 @@ const HeroSection = styled.section`
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
-    opacity: 0.12; 
+    opacity: 0.15; 
     z-index: -2;
   }
   

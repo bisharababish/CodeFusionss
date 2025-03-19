dd90ed97be5c0039a9c0f916814e8fe998cca4cc
bisharababish <62569803+bisharababish@users.noreply.github.com>
2025-03-19 13:09:53 +0200

    s

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Analytics } from "@vercel/analytics/react"
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );

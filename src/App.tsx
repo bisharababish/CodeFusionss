788b5e090da36e9d532ff82caf022bd63ad24e31
bisharababish <62569803+bisharababish@users.noreply.github.com>
2025-10-10 20:57:31 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Home from './pages/Home';
 import ProjectsPage from './pages/ProjectsPage';
 import AboutPage from './pages/AboutPage';
 import TeamsPage from './pages/TeamsPage';
-import PDFPage from './pages/PDFPage';
 
 const App: React.FC = () => {
   return (
@@ -21,7 +20,6 @@ const App: React.FC = () => {
           <Route path="/projects" element={<ProjectsPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/team" element={<TeamsPage />} />
-          <Route path="/pdf/:pdfName" element={<PDFPage />} />
         </Routes>
         <Footer />
       </div>
